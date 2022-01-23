128bf10d1b821e03d4dc23634fbd74afbf0b231b
roach <tmdgusya@naver.com>
2022-01-23 13:27:17 +0900

    init

---
+++ src/main/kotlin/com/example/csvexample/CsvExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.csvexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsvExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsvExampleApplication>(*args)
+}
