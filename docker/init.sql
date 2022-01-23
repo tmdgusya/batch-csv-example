ab6ed4203e6dacf78be08b461c70b9cde5e6d0aa
roach <tmdgusya@naver.com>
2022-01-23 13:41:50 +0900

    init sql script

---
+++ docker/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE user(
+    id int(11) primary key AUTO_INCREMENT NOT NULL,
+    name VARCHAR(20),
+    email VARCHAR(20)
+)
+
+INSERT INTO user (name, email) values ('로치', 'dev0jsh@gmail.com');
+INSERT INTO user (name, email) values ('두더지', 'doo@gmail.com');
+INSERT INTO user (name, email) values ('푸', 'foo@gmail.com');
+INSERT INTO user (name, email) values ('포우', 'pow@gmail.com');
+INSERT INTO user (name, email) values ('태리', 'teri@gmail.com');
+INSERT INTO user (name, email) values ('호돌', 'hodol@gmail.com');
+INSERT INTO user (name, email) values ('도도', 'dodo@gmail.com');
